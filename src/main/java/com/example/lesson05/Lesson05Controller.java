1156747f45f3ffa1121c361365240919c56ffb93
DESKTOP-61D76IS\user <forthewy@gmail.com>
2022-09-28 21:42:34 +0900

    0928 수업내용 JSTL

---
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -0,0 +1,51 @@
+package com.example.lesson05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/lesson05")
+@Controller
+public class Lesson05Controller {
+	
+	@RequestMapping("/ex01")
+	public String ex01() {
+		return "lesson05/ex01";
+	}
+	
+	@RequestMapping("/ex02")
+	public String ex02(Model model) {
+		// List<String> 
+		List<String> fruits = new ArrayList<>();
+		fruits.add("apple");
+		fruits.add("melon");
+		fruits.add("peach");
+		fruits.add("grape");
+		fruits.add("mango");
+		
+		model.addAttribute("fruits", fruits);
+		
+		
+		// List<Map>
+		List<Map<String, Object>> users = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", "김인간");
+		map.put("age", "50");
+		map.put("hobby", "보드타기");
+		users.add(map);
+		
+		map = new HashMap<>();
+		map.put("name", "이휴먼");
+		map.put("age", "25");
+		map.put("hobby", "독서");
+		users.add(map);
+		
+		model.addAttribute("users", users);
+		return "lesson05/ex02";
+	}
+}
